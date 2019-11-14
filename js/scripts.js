e0f4878c3e33761d1635c983b94cd5c9af25e472
JasonT <jthompson@duoconsulting.com>
2019-11-14 11:00:58 -0600

    TT-16: Added user login styles and some js to support.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@
  */
 (function ($, Drupal) {
 
+
   /**
    * Use this behavior as a template for custom Javascript.
    */
@@ -29,6 +30,34 @@
     // Hide the header as the user scrolls down and then show it again as the
     // user scrolls up.
     headerUpDown();
+
+    // Drupal core toolbar module sets the padding-top initially. 
+    // But this doesn't work with mini admin toolbar, so handle here.
+    function toolbarOffset() { 
+      // First get height of toolbar and tray. 
+      var $toolbar_barHeight = $(".toolbar-bar");
+      var $toolbar_trayHeight = $(".toolbar-tray");
+      var barHeight = $toolbar_barHeight.outerHeight();
+      var trayHeight= $toolbar_trayHeight.outerHeight();
+      var fullBarHeight = barHeight + trayHeight;
+
+      if (window.matchMedia('(min-width: 610px)').matches) {
+        // And only do this when logged in of Course... 
+        if ($('.toolbar-themes').length && ($('.toolbar-tray.is-active').length)) {
+          // Both bar and tray showing
+          $('body').css({'padding-top': fullBarHeight+ 'px'});
+        } else if ($('.toolbar-themes').length) {
+          // Just the bar is showing
+          $('body').css({'padding-top': barHeight + 'px'});
+        }
+      } else {
+        // On small devices toolbar is position: relative - so do nothing.
+      }
+    };
+
+    // Call the function now, and on resize:
+    toolbarOffset();
+    $(window).on('resize', Foundation.util.throttle(toolbarOffset, 300));
 
   });
 
