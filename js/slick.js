5129bedac44ce978b1125adfae26a6a613f90a7f
JasonT <jthompson@duoconsulting.com>
2020-02-22 11:36:31 -0600

    TT-52: Twig and Style refinements for slideshow.

--- js/slick.js
+++ js/slick.js
@@ -16,23 +16,48 @@
 
         var totalSlides = $(this).find(".slick-hero .slide").length;
 
-        $(this).find(".slick-hero").slick(
-          {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            arrows: true,
-            fade: true
-          }
-        );
+        // Slick hero
+        $(this).find(".slick-hero").slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: true,
+          dots: true,
+          fade: true,
+          asNavFor: $(this).find(".slick-hero-nav-thumbnails")
+        })
+          // All slides same height.
+          .on('setPosition', function (event, slick) {
+            slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+          });
+
         //  Slick navigation 
         $(this).find(".slick-hero-nav-thumbnails").slick({
           slidesToShow: totalSlides,
           slidesToScroll: 1,
           asNavFor: $(this).find(".slick-hero"),
+          arrows: false,
           dots: false,
           focusOnSelect: true
-        });
+        })
+          // All slides same height.
+          .on('setPosition', function (event, slick) {
+            slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+          });
+
+        // Slick with dots
+        $(this).find(".slick-with-dots").slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: true,
+          fade: true
+        })
+          // All slides same height.
+          .on('setPosition', function (event, slick) {
+            slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+          });
       });
     }
   };
